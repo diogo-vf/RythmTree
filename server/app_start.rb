355b10e7fcf62700d233cd9ad75283f17fb682a7
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-01-12 17:30:12 +0100

    created watcher script

--- server/app_start.rb
+++ server/app_start.rb
@@ -1,12 +1,4 @@
 require_relative "http_server/listener"
 require_relative "websocket_server/listener"
 
-while
-    begin
-        HTTPServer.start
-    rescue => exception
-        puts "the script crashed #{exception.to_s}"
-        sleep 1
-        puts "restarting..."
-    end
-end
+HTTPServer.start
