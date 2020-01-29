fedbe31ac42cfd32e3503c6ae354e096d97e231b
HALDY Alexis <alexishaldy@gmail.com>
2020-01-29 08:42:20 +0100

    Improve play/pause button and select list

--- client/scripts/adapters.js
+++ client/scripts/adapters.js
@@ -17,6 +17,14 @@ function Adapters(){
 
 	// Level Editor Adapter
 	this.levelEditorAdapter = function(container, data) {
+		if (!container.innerText) {
+			let option = container.addElement("option");
+			option.value = "";
+			option.selected = true;
+			option.hidden = true;
+			option.disabled = true;
+			option.textContent = "Choisir...";
+		}
 		const option = container.addElement("option"),
 			minTime = (data.duration/60000 < 10 ? "0" : "") + Math.round(data.duration/60000),
 			secTime = (data.duration%60000 < 10 ? "0" : "") + Math.round((data.duration%60000)/1000) + (Math.round((data.duration%60000)/1000) === 1 ? "0" : "");
